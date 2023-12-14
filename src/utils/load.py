8cba2b4325575d7e21a2ab7b00ce52f623313460
Muspi Merol <wjj123345567@icloud.com>
2023-12-15 02:35:27 +0800

    Implement a new syntactic sugar

--- src/utils/load.py
+++ src/utils/load.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Annotated, Literal
 
 from promplate.prompt.utils import get_builtins
+from typing_extensions import Self
 
 from .helpers import DotTemplate
 
@@ -10,7 +11,11 @@ root = Path("src/templates")
 
 
 def load_template(stem: str):
-    return DotTemplate.read(glob()[stem])
+    try:
+        return DotTemplate.read(glob()[stem])
+    except KeyError:
+        if (root / stem).is_dir():
+            return getattr(components, stem)
 
 
 def generate_pyi():
@@ -34,11 +39,27 @@ if not __debug__ and not TYPE_CHECKING:
 
 
 class LazyLoader(dict):
+    path = root
+
     def __missing__(self, key):
         try:
             return load_template(key)
         except FileNotFoundError:
             raise KeyError(f"Prompt {key} not found")
+
+    def __getattr__(self, stem: str) -> Self | DotTemplate:
+        if (root / stem).is_dir():
+            loader = LazyLoader()
+            loader.path = self.path / stem
+            return loader
+
+        return self[(self.path / stem).relative_to(root).as_posix()]
+
+    if not __debug__:
+        __getattr__ = cache(__getattr__)
+
+    def __hash__(self):
+        return hash(self.path)
 
 
 components = LazyLoader(get_builtins())  # avoid shadowing builtins
