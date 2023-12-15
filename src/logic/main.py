c291d52015e4e5d0cd20d48411f0714b0366172b
Muspi Merol <wjj123345567@icloud.com>
2023-12-15 11:38:20 +0800

    Add JSON parser to `</main/>` Inject tools description into prompts Modify prompts Bind `default_box=True` into `SilentBox`

--- src/logic/main.py
+++ src/logic/main.py
@@ -1,5 +1,7 @@
-from promplate import Node
+from promplate import ChainContext, Node
+from promptools.extractors import extract_json
 
+from ..templates.schema.output import Output
 from ..utils.llm.openai import openai
 from ..utils.load import load_template
 
@@ -7,3 +9,8 @@ main = Node(load_template("main"), llm=openai)
 
 
 main.add_pre_processes(print)
+
+
+@main.post_process
+def parse_json(context: ChainContext):
+    context["parsed"] = extract_json(context.result, {}, Output)
