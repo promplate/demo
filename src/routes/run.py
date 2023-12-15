435adbe5659ac408fd837285b72efdcc78091574
Muspi Merol <wjj123345567@icloud.com>
2023-12-15 10:29:10 +0800

    Add exception handler and rename tags

--- src/routes/run.py
+++ src/routes/run.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 from promplate import Context
 
 from ..logic import get_node
 from ..utils.load import Template
 
-run_router = APIRouter(tags=["run"])
+run_router = APIRouter(tags=["call"])
 
 
 @run_router.post("/invoke/{node:path}")
 async def invoke(node: Template, context: Context):
     n = get_node(node)
-    return await n.ainvoke(context)
+    try:
+        return await n.ainvoke(context)
+    except Exception as e:
+        return PlainTextResponse(str(e), 500)
