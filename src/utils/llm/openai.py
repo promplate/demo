a73ce2341cbfbfa0bec5dbea18e7a4550f85b695
Muspi Merol <wjj123345567@icloud.com>
2023-12-18 11:37:17 +0800

    Clear the environment after initialization for safety reason

--- src/utils/llm/openai.py
+++ src/utils/llm/openai.py
@@ -1,8 +1,15 @@
+from os import environ
+
 from httpx import AsyncClient
 from promplate.llm.openai import AsyncChatOpenAI
 
-openai = AsyncChatOpenAI(http_client=AsyncClient(http2=True)).bind(
+from ..config import env
+
+openai = AsyncChatOpenAI(http_client=AsyncClient(http2=True), api_key=env.openai_api_key, base_url=env.openai_base_url).bind(
     model="gpt-3.5-turbo-1106",
     temperature=0.2,
     # response_format={"type": "json_object"},
 )
+
+
+environ.clear()
