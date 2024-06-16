9b1a9bd633e8fd4e52f3f9a8d15ac3e8ec88f092
Muspi Merol <wjj123345567@icloud.com>
2024-06-16 15:21:55 +0800

    feat: ensure messages format to be valid for Claude

--- src/utils/llm/common.py
+++ src/utils/llm/common.py
@@ -12,8 +12,28 @@ class SafeMessage(TypedDict):
     content: str
 
 
-def ensure_safe(prompt: str | list[Message]):
+def ensure_no_system(prompt: str | list[Message]):
     return [
         cast(SafeMessage, {"role": i["role"] if i["role"] != "system" else "user", "content": i["content"]})
         for i in ensure(prompt)
     ]
+
+
+def join_adjacent_inplace(messages: list[SafeMessage]):
+    i = 1
+    while i < len(messages):
+        if messages[i - 1]["role"] == messages[i]["role"]:
+            if messages[i - 1]["content"] != messages[i]["content"]:
+                messages[i - 1]["content"] += "\n\n" + messages[i]["content"]
+            messages.pop(i)
+        else:
+            i += 1
+
+
+def ensure_safe(prompt: str | list[Message]):
+    messages = ensure_no_system(prompt)
+    join_adjacent_inplace(messages)
+    if messages[0]["role"] == "assistant":
+        messages.insert(0, {"role": "user", "content": "..."})
+
+    return messages
