b1ad0423f397921eb974632bbef3a6dc15fcc04b
Muspi Merol <wjj123345567@icloud.com>
2024-08-09 07:38:44 +0800

    fix: provide `Fira Code` font

--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -1,10 +1,10 @@
 import extractorSvelte from "@unocss/extractor-svelte";
-import { defineConfig, presetAttributify, presetUno, transformerDirectives, transformerVariantGroup } from "unocss";
+import { defineConfig, presetAttributify, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from "unocss";
 
 const config = defineConfig({
   extractors: [extractorSvelte()],
   transformers: [transformerDirectives(), transformerVariantGroup()],
-  presets: [presetAttributify(), presetUno()],
+  presets: [presetAttributify(), presetUno(), presetWebFonts({ provider: "bunny", fonts: { mono: "Fira Code" } })],
 });
 
 export default config;
