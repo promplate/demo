a40ce4813015e7e5fde2b96d66a43f560de00baa
Muspi Merol <me@promplate.dev>
2024-12-26 23:11:05 +0800

    feat: configure logfire with code source and environment settings

--- src/__init__.py
+++ src/__init__.py
@@ -9,8 +9,9 @@ from .utils.config import env
 
 if env.logfire_token:
     import logfire
+    from langfuse.environment import get_common_release_envs
 
-    logfire.configure()
+    logfire.configure(code_source=logfire.CodeSource("https://github.com/promplate/demo", get_common_release_envs() or "deploy"))
     logfire.info("app started", **env.model_dump())
     logfire.instrument_httpx()
     logfire.instrument_openai()
