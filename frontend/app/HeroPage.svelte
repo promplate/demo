715d52d90c27d75f1656d8b8f7d22e8c3a656ce4
Muspi Merol <me@promplate.dev>
2025-02-18 17:12:34 +0800

    chore: replace deprecated `tweened` with `Tween`

--- frontend/app/HeroPage.svelte
+++ frontend/app/HeroPage.svelte
@@ -2,18 +2,18 @@
   import { TokenSquare } from "@promplate/pattern";
   import { onMount } from "svelte";
   import { sineInOut } from "svelte/easing";
-  import { tweened } from "svelte/motion";
+  import { Tween } from "svelte/motion";
 
   const length = 20;
-  const i = tweened(length * 1.3, { easing: sineInOut, duration: 700 });
+  const i = new Tween(length * 1.3, { easing: sineInOut, duration: 700 });
 
-  onMount(() => ($i = 0));
+  onMount(() => i.set(0));
 </script>
 
 <div class="grid h-screen select-none place-items-center bg-neutral-950">
   <div class="flex flex-col gap-2 text-neutral-2">
     <div class="text-sm">
-      <TokenSquare {length} isHighlighted={({ x, y }) => y - x >= $i} />
+      <TokenSquare {length} isHighlighted={({ x, y }) => y - x >= i.current} />
     </div>
     <h1 class="mx-0.5 w-fit text-white font-sans">
       Promplate
