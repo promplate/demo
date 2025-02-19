041d79c60dbcbe7f5239a652f2f5a503cc2face2
Muspi Merol <me@promplate.dev>
2025-02-19 21:36:28 +0800

    chore: update llm from groq provider

--- src/utils/llm/groq.py
+++ src/utils/llm/groq.py
@@ -18,6 +18,8 @@ generate = AsyncChatGenerate(http_client=client, base_url=env.groq_base_url, api
 @link_llm("llama-3.2")
 @link_llm("llama-3.3-70b-")
 @link_llm("mixtral")
+@link_llm("qwen-2.5")
+@link_llm("deepseek-r1-distill")
 class Groq(AsyncChatOpenAI):
     async def complete(self, prompt: str | list[Message], /, **config):
         config = self._run_config | config
