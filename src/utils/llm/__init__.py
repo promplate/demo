918a8562b7a97dfccb08bdb21f62ba65f9a9761f
Muspi Merol <me@promplate.dev>
2025-02-23 00:24:48 +0800

    chore: update llm from sambanova provider

--- src/utils/llm/__init__.py
+++ src/utils/llm/__init__.py
@@ -134,5 +134,12 @@ Model = Literal[
     "Qwen2.5-Coder-32B-Instruct",
     "Qwen2.5-72B-Instruct",
     "QwQ-32B-Preview",
+    "Llama-3.1-Tulu-3-405B",
+    "Llama-3.2-11B-Vision-Instruct",
+    "Llama-3.2-90B-Vision-Instruct",
+    "Meta-Llama-3.2-1B-Instruct",
+    "Meta-Llama-3.2-3B-Instruct",
+    "Meta-Llama-3.3-70B-Instruct",
+    "DeepSeek-R1-Distill-Llama-70B",
     "deepseek-chat",
 ]
--- src/utils/llm/sambanova.py
+++ src/utils/llm/sambanova.py
@@ -8,6 +8,10 @@ from .dispatch import link_llm
 
 @link_llm("Qwen2.5")
 @link_llm("QwQ")
+@link_llm("Llama-3")
+@link_llm("Meta-Llama-3.2")
+@link_llm("Meta-Llama-3.3")
+@link_llm("DeepSeek-R1-Distill")
 class SambaNova(AsyncChatOpenAI):
     complete = staticmethod(
         patch.chat.acomplete(
