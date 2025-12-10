8d015e36cf2b021f06f4f8853ed0620d4463825e
Muspi Merol <me@promplate.dev>
2025-12-11 04:15:59 +0800

    chore: update model literals from OpenRouter

--- src/utils/llm/openrouter.py
+++ src/utils/llm/openrouter.py
@@ -17,19 +17,16 @@ llm = AsyncChatOpenAI(
 )
 
 
-@link_llm("microsoft/mai-ds-r1:free")
-@link_llm("deepseek/deepseek-r1-0528:free")
-@link_llm("deepseek/deepseek-chat-v3-0324:free")
 @link_llm("z-ai/glm-4.5-air:free")
 @link_llm("qwen/qwen3-coder:free")
 @link_llm("kwaipilot/kat-coder-pro:free")
 @link_llm("alibaba/tongyi-deepresearch-30b-a3b:free")
-@link_llm("meituan/longcat-flash-chat:free")
 @link_llm("allenai/olmo-3-32b-think:free")
 @link_llm("arcee-ai/trinity-mini:free")
 @link_llm("tngtech/tng-r1t-chimera:free")
 @link_llm("amazon/nova-2-lite-v1:free")
 @link_llm("mistralai/devstral-2512:free")
+@link_llm("nex-agi/deepseek-v3.1-nex-n1:free")
 class OpenRouter(AsyncChatOpenAI):
     complete = staticmethod(patch.chat.acomplete(llm.complete))
     generate = staticmethod(patch.chat.agenerate(llm.generate))
